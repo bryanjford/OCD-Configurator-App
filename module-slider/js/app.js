7509a118bd63f51cfa62858ae425d0c6ac1fea1b
Bryan Ford <bryanjford@crystalpix.com>
2015-06-10 10:19:28 -0400

    first file commit

--- module-slider/js/app.js
+++ module-slider/js/app.js
@@ -2,6 +2,12 @@ window.onload = function(){
     
     
     var compSlider = document.getElementById("slider");
+    var currentCompCount = {};
+    var componentData = null;
+    var detailDisplayPanelEl = document.getElementById("detail-panel");
+    var overlayPanelEl = document.getElementById("overlay-panel");
+
+    
     
     
     var dataReq = new XMLHttpRequest();
@@ -13,7 +19,9 @@ window.onload = function(){
         function onModuleDataLoaded(e){
         data = JSON.parse(e.currentTarget.response);
         componentData = (data.components);
+        currentCompCount = (data.componentCount);
         initModuleConstructor(componentData)
+        initSlidePanel(currentCompCount)
         }
     
     
@@ -24,7 +32,7 @@ window.onload = function(){
     
         function initModuleConstructor(compData){ // creates components lists / passes them thru "sortModules()"
 
-            
+
             var preAnalyticalData = compData.preAnalytical;
             var analyticalData = compData.analytical;
             var postAnalyticalData = compData.postAnalytical;
@@ -34,8 +42,6 @@ window.onload = function(){
             var postAnalyticalList = createComponentList(postAnalyticalData);
             
             sortModules(preAnalyticalList,analyticalList,postAnalyticalList);
-
-            
         }
     
         function sortModules(preAnList,anList,postAnList){  // appends component panels into respective module divs
@@ -55,23 +61,28 @@ window.onload = function(){
             var moduleListEl = document.createElement("div");
             
             for (component in compList){
-                var slidePanel = createComponentEl(compList[component]);
+                var slidePanel = createComponentEl(compList[component],component);
                 moduleListEl.appendChild(slidePanel);
+                //console.log(component);
             }
             
             return moduleListEl;
-            
         }
         
-        function createComponentEl(compData){
-            //console.log(compData);
+        function createComponentEl(compData,compId){
+            console.log(compData);
             
             var compEl = document.createElement("div");
+            var compDetailContainer = document.createElement("div");
+            var compLabel = document.createElement("p");
+            var infoBtn = document.createElement("div");
+            var infoBorder = document.createElement("div");
             var compThumb = document.createElement("img");
-            var compLabel = document.createElement("h1");
-            var moduleTitlebar = document.createElement("div");
-            
-            moduleTitlebar.className = "titleBar";
+
+            infoBtn.className = "infoBtn";
+            infoBtn.setAttribute("compId",compId);
+            infoBtn.setAttribute("modType",compData.modFlag);
+            infoBtn.addEventListener("click", onInfoBtnClick);
             compEl.className = "comp";    
             compEl.setAttribute('id',compData.name);
             compThumb.setAttribute('draggable', false);
@@ -79,10 +90,15 @@ window.onload = function(){
             
             compThumb.src = compData.thumbnail;
             compLabel.innerHTML = compData.name
-
-            compEl.appendChild(moduleTitlebar);    
+            compDetailContainer.appendChild(compLabel);
+            compDetailContainer.appendChild(infoBtn);
+            compDetailContainer.className = "comp-details";
+            
+            compEl.style.marginLeft = compData.margin.left +"px";
+            compEl.style.marginRight = compData.margin.right +"px";
+    
+            compEl.appendChild(compDetailContainer);
             compEl.appendChild(compThumb);
-            compEl.appendChild(compLabel);
             
             function disableSelect(e){ //sets textfields as unselectable
                 return false;   
@@ -96,8 +112,107 @@ window.onload = function(){
         //////// component-slider functions ///////
     
     
-        
-        
     
+    function initSlidePanel(compCount){
+    
+        var compWidth = 450;
+        var sliderWidth = compSlider.style.width =(compWidth * (currentCompCount+1))+"px";
+        
+    }
+    
+    
+        //////// detail-panel functions ///////
+    
+    
+    function onInfoBtnClick(e){
+        //console.log(e.currentTarget);
+        var selectedComp = e.currentTarget.getAttribute("compId");
+        var selectedModuleType = e.currentTarget.getAttribute("modType");
+        
+        var currentDetailComp = componentData[selectedModuleType][selectedComp];
+        
+        initDetailPanel(currentDetailComp);
+        detailDisplayPanelEl.style.display = "block";
+        overlayPanelEl.style.display = "block";
+    }
+    
+    function initDetailPanel(currentComp){
+        //console.log(currentComp);
+        var detailPanelEl = document.getElementById("detail-panel");
+        detailPanelEl.innerHTML = "";
+        
+        var panelCloseBtn = document.createElement("div");
+        panelCloseBtn.setAttribute("id","close-btn");
+        panelCloseBtn.addEventListener("click",onPanelCloseClick);
+        
+        var panelHeader = document.createElement("div");
+        panelHeader.setAttribute("id","panel-header");
+        
+        var nameDiv = document.createElement("div");
+            nameDiv.setAttribute("id","detail-name-container");
+        var nameEl = document.createElement("p");
+            nameEl.innerHTML = currentComp.name
+            nameDiv.appendChild(nameEl);
+        
+        var moduleDiv = document.createElement("div");
+            moduleDiv.setAttribute("id","detail-mod-container");
+        var moduleEl = document.createElement("p");
+            moduleEl.innerHTML = currentComp.modLabel;
+            moduleDiv.appendChild(moduleEl);
+        
+        var compImgEl = document.createElement("img");
+            compImgEl.src = currentComp.thumbnail;
+        
+        var featureHeaderEl = document.createElement("h3");
+            featureHeaderEl.innerHTML = currentComp.featureHeader;
+        
+        var featureListDiv = document.createElement("div");
+            featureListDiv.setAttribute("id","features");
+            featureListDiv.appendChild(featureHeaderEl);
+        var featureListEl = createDetailList(currentComp.featureList);
+            featureListDiv.appendChild(featureListEl);
+        
+        var videoPlayBtn = document.createElement("div");
+        var playBtnTxt = document.createElement("p");
+        playBtnTxt.innerHTML = "Touch To Play Video";
+        videoPlayBtn.appendChild(playBtnTxt);
+        videoPlayBtn.setAttribute("id","video-btn");
+        videoPlayBtn.addEventListener("click",onVideoPlayClick);
+        
+
+        detailPanelEl.appendChild(panelCloseBtn);
+        panelHeader.appendChild(nameDiv);
+        panelHeader.appendChild(moduleDiv);
+        detailPanelEl.appendChild(panelHeader);
+        detailPanelEl.appendChild(compImgEl);
+        detailPanelEl.appendChild(featureListDiv);
+        detailPanelEl.appendChild(videoPlayBtn);
+                    
+        
+    }
+    
+    function createDetailList(list){
+        console.log(list);
+        var detailFeatureList = document.createElement("ul");
+        
+        for(item in list){
+            var listItemEl = document.createElement("li");
+            listItemEl.innerHTML = list[item];
+            detailFeatureList.appendChild(listItemEl);
+        }
+        
+        return detailFeatureList;
+        
+    }
+    
+    function onPanelCloseClick(e){
+        console.log("close panel clicked");
+        detailDisplayPanelEl.style.display = "none";
+        overlayPanelEl.style.display = "none";
+    }
+    
+    function onVideoPlayClick(e){
+        console.log("video btn clicked");
+    }
 
 }
